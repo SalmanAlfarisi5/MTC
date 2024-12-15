7075a96c34a9de913317f45be909ade62b809986
Salman <salman26080@gmail.com>
2024-12-15 08:38:01 +0800

    Finished drafting for contact
    the form also working propoerly

--- src/Components/Contact/Contact.jsx
+++ src/Components/Contact/Contact.jsx
@@ -4,8 +4,34 @@ import msg_icon from '../../assets/msg-icon.png'
 import mail_icon from '../../assets/mail-icon.png'
 import phone_icon from '../../assets/phone-icon.png'
 import location_icon from '../../assets/location-icon.png'
+import white_arrow from '../../assets/white-arrow.png'
 
 const Contact = () => {
+
+  const [result, setResult] = React.useState("");
+
+  const onSubmit = async (event) => {
+    event.preventDefault();
+    setResult("Sending....");
+    const formData = new FormData(event.target);
+
+    formData.append("access_key", "f7d7ef7a-524d-4308-97c8-561b559bf08f");
+
+    const res = await fetch("https://api.web3forms.com/submit", {
+      method: "POST",
+      body: formData
+    }).then((res) => res.json());
+
+    if (res.success) {
+      console.log("Success", res)
+      setResult(res.message);
+      event.target.reset();
+    } else {
+      console.log("Error", res);
+      setResult(res.message);
+    }
+  };
+
   return (
     <div className='contact'>
         <div className="contact-col">
@@ -21,7 +47,18 @@ const Contact = () => {
                 <li> <img src={location_icon} alt="" />Cibubur Indah V </li>
             </ul>
         </div>
-        <div className="contact-col"></div>
+        <div className="contact-col">
+          <form onSubmit={onSubmit}>
+            <label>Your name</label>
+            <input type="text" name = 'name' placeholder='Enter your name' required/>
+            <label>Phone number</label>
+            <input type="tel" name='phone' placeholder='Enter your mobile number' required/>
+            <label>Write your message here</label>
+            <textarea name="message" rows='6' placeholder='Enter your message' required></textarea>
+            <button type='submit' className='btn dark-btn'>Submit now <img src={white_arrow} alt="" /></button>
+          </form>
+          <span>{result}</span>
+        </div>
     </div>
   )
 }
