eccb9ade94c079e044b957d4cd4c930539bb5811
Salman <salman26080@gmail.com>
2024-12-22 14:32:26 +0800

    Added Whatsapp feature and config to the web

--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,35 @@
-import React, { useState } from 'react'
-import Navbar from './Components/Navbar/Navbar'
-import Hero from './Components/Hero/Hero'
-import Programs from './Components/Programs/Programs'
-import Title from './Components/Title/Title'
-import About from './Components/About/About'
-import Clients from './Components/Clients/Clients'
-import Teams from './Components/Teams/Teams'
-import Contact from './Components/Contact/Contact'
-import Footer from './Components/Footer/Footer'
+import React from 'react';
+import Navbar from './Components/Navbar/Navbar';
+import Hero from './Components/Hero/Hero';
+import Programs from './Components/Programs/Programs';
+import Title from './Components/Title/Title';
+import About from './Components/About/About';
+import Clients from './Components/Clients/Clients';
+import Teams from './Components/Teams/Teams';
+import Contact from './Components/Contact/Contact';
+import Footer from './Components/Footer/Footer';
+import WhatsappWidget from './Components/WhatsappWidget/WhatsappWidget';
 
 const App = () => {
-
   return (
     <div>
-      <Navbar/>
-      <Hero/>
+      <Navbar />
+      <Hero />
       <div className="container">
-        <Title subTitle='Our programs' title = 'What we offer'/>
-        <Programs/>
-        <About/>
-        <Title subTitle='Clients' title = 'Companies that Choose Us'/>
-        <Clients/>
-        <Title subTitle='' title = 'Our Teams'/>
-        <Teams/>
-        <Title subTitle='Contact Us' title = 'Get in Touch'/>
-        <Contact/>
-        <Footer/>
+        <Title subTitle="Our programs" title="What we offer" />
+        <Programs />
+        <About />
+        <Title subTitle="Clients" title="Companies that Choose Us" />
+        <Clients />
+        <Title subTitle="" title="Our Teams" />
+        <Teams />
+        <Title subTitle="Contact Us" title="Get in Touch" />
+        <Contact />
+        <Footer />
       </div>
+      <WhatsappWidget /> 
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default App;
