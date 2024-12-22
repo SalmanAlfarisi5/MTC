e6a99541e9d71e0bd0074349d2c770e98da184d0
Salman <salman26080@gmail.com>
2024-12-22 14:12:36 +0800

    Updated Hero and logo

--- src/Components/Hero/Hero.jsx
+++ src/Components/Hero/Hero.jsx
@@ -1,16 +1,11 @@
 import React from 'react'
 import './Hero.css'
 
-
 const Hero = () => {
   return (
     <div className='hero container'>
         <div className="hero-text">
             <h1>Tax Consultation for your Businesses</h1>
-            <p> Tax consulting company located in Jakarta that has obtained 
-              official certification and has experience in handling tax 
-              obligations for taxpayers
-            </p>
             <a href="https://drive.google.com/file/d/1VKNeaj9ISnw_zEUqvzgxsu3fbFvhX7O2/view?usp=sharing" 
             target="_blank" rel="noopener noreferrer" className="btn"> Explore more 
             </a>
