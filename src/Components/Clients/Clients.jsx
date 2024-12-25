aead1896142b10f26a23bc3cc103a8eadb967223
Salman <salman26080@gmail.com>
2024-12-25 22:32:52 +0800

    Updated the code

--- src/Components/Clients/Clients.jsx
+++ src/Components/Clients/Clients.jsx
@@ -1,6 +1,26 @@
 import React from "react";
 import "./Clients.css";
-import clientsData from "../../data/client.json";
+import photo1 from "../../assets/photo-1.png";
+import photo2 from "../../assets/photo-2.png";
+import photo3 from "../../assets/photo-3.png";
+import photo4 from "../../assets/photo-4.png";
+import photo5 from "../../assets/photo-5.png";
+import photo6 from "../../assets/photo-6.png";
+import photo7 from "../../assets/photo-7.png";
+import photo8 from "../../assets/photo-8.png";
+import photo9 from "../../assets/photo-9.png";
+
+const clientsData = [
+  { image: photo1, name: "Arsolusi Consulting Group" },
+  { image: photo2, name: "Cinta Puri Pratama" },
+  { image: photo3, name: "Garsindo Engineering Equipment" },
+  { image: photo4, name: "CV. Rahmania Bagaskara Gemilang" },
+  { image: photo5, name: "Amanah Tour" },
+  { image: photo6, name: "Prakasita Citra Buana" },
+  { image: photo7, name: "Rekamitra Cipta Tambang" },
+  { image: photo8, name: "Rekamitra Sukses Investama" },
+  { image: photo9, name: "PT. Transworld Solution" },
+];
 
 const Clients = () => {
   return (
@@ -9,11 +29,9 @@ const Clients = () => {
         {clientsData.map((client, index) => (
           <div className="client" key={index}>
             <img src={client.image} alt={client.name} />
-            <p>{client.name}</p>
           </div>
         ))}
       </div>
-      <button className="btn dark-btn">See more here</button>
     </div>
   );
 };
