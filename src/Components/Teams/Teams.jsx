9937e1a9b211099ea74cc4bc9c6f866b5ad31c3a
Salman <salman26080@gmail.com>
2025-01-20 18:35:51 +0800

    Updated team to be auto scroll

--- src/Components/Teams/Teams.jsx
+++ src/Components/Teams/Teams.jsx
@@ -1,4 +1,4 @@
-import React, { useRef, useState } from "react";
+import React, { useRef, useState, useEffect } from "react";
 import "./Teams.css";
 import next_icon from "../../assets/next-icon.png";
 import back_icon from "../../assets/back-icon.png";
@@ -91,11 +91,26 @@ const Teams = () => {
   const [modalVisible, setModalVisible] = useState(false);
   const [modalContent, setModalContent] = useState({ text: "", img: "" });
   const { language } = useLanguage(); 
+  const [direction, setDirection] = useState("right");
   let tx = 0;
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      if (direction === "right") {
+        slideForward();
+      } else {
+        slideBackward();
+      }
+    }, 3000);
+
+    return () => clearInterval(interval);
+  }, [direction]);
 
   const slideForward = () => {
     if (tx > -60) {
       tx -= 20;
+    } else {
+      setDirection("left");
     }
     slider.current.style.transform = `translateX(${tx}%)`;
   };
@@ -103,6 +118,8 @@ const Teams = () => {
   const slideBackward = () => {
     if (tx < 0) {
       tx += 20;
+    } else {
+      setDirection("right");
     }
     slider.current.style.transform = `translateX(${tx}%)`;
   };
